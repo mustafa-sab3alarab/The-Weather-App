c8fe3af894a0ec920190d18104273b55165e4842
Mustafa <mustafa777alarab@gmail.com>
2021-10-02 01:57:24 +0300

    add Adapter to my own recycler view

---
+++ app/src/main/java/com/example/ad340/DailyForecastAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.ad340
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DailyForecastAdapter : RecyclerView.Adapter<DailyForecastAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
